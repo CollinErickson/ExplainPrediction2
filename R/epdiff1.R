63c8d8c0032128bad3376af0013a38a1bae1a938
Collin Erickson <collinberickson@gmail.com>
2021-07-10 20:40:50 -0400

    Added predictfuncwithmod

--- R/epdiff1.R
+++ R/epdiff1.R
@@ -5,6 +5,8 @@
 #' @param x2 Input 2. Single row data frame or matrix.
 #' @param d Max number of input dimensions to use.
 #' @param predictfunc Func to use to predict. Defaults to `predict`.
+#' Takes in model and data.
+#' @param predictfuncwithmod Func to use to predict. Only takes in data.
 #'
 #' @return ggplot object showing effect
 #' @export
@@ -18,6 +20,11 @@
 #' epdiff1(m1, x1, x2)
 #' epdiff1(m1, iris[12,], iris[20,])
 #' epdiff1(m1, iris[sample(1:nrow(iris), 1),], iris[sample(1:nrow(iris), 1),])
+#' epdiff1(m1, iris[sample(1:nrow(iris), 1),], iris[sample(1:nrow(iris), 1),], predictfunc=predict)
+#' epdiff1(m1, iris[sample(1:nrow(iris), 1),], iris[sample(1:nrow(iris), 1),],
+#'     predictfuncwithmod=function(xx){suppressMessages(predict(m1, xx))})
+#' epdiff1(NA, iris[sample(1:nrow(iris), 1),], iris[sample(1:nrow(iris), 1),],
+#'     predictfuncwithmod=function(xx){suppressMessages(predict(m1, xx))})
 #'
 #' # Linear model
 #' mod_lm <- lm(Petal.Width ~ Sepal.Length + Sepal.Width, data=iris)
@@ -25,17 +32,22 @@
 #' epdiff1(mod_lm, iris[sample(1:nrow(iris), 1),], iris[sample(1:nrow(iris), 1),], d=3)
 #' mod_lm2 <- lm(Petal.Width ~ Petal.Length + Sepal.Width, data=iris)
 #' epdiff1(mod_lm2, iris[sample(1:nrow(iris), 1),], iris[sample(1:nrow(iris), 1),])
-epdiff1 <- function(mod, x1, x2, d, predictfunc) {
+epdiff1 <- function(mod, x1, x2, d, predictfunc, predictfuncwithmod) {
   stopifnot(nrow(x1)==1, nrow(x2)==1, ncol(x1)==ncol(x2))
 
   if (missing(predictfunc)) {
     predictfunc <- predict
   }
+  if (missing(predictfuncwithmod)) {
+    predictfuncwithmod <- function(x) {suppressMessages(predictfunc(mod, x))}
+  }
 
   # predict(m1, x1)
   # predict(m1, x2)
-  y1 <- suppressMessages(predictfunc(mod, x1))
-  y2 <- suppressMessages(predictfunc(mod, x2))
+  # y1 <- suppressMessages(predictfunc(mod, x1))
+  # y2 <- suppressMessages(predictfunc(mod, x2))
+  y1 <- predictfuncwithmod(x1)
+  y2 <- predictfuncwithmod(x2)
   # varuse <- rep(NA, ncol(x1))
   # xt <- x1
   # indexorder <- c()
@@ -52,8 +64,10 @@ epdiff1 <- function(mod, x1, x2, d, predictfunc) {
     xi_1to2[i] <- x2[i]
     xi_2to1 <- x2
     xi_2to1[i] <- x1[i]
-    yi_1to2 <- suppressMessages(predictfunc(mod, xi_1to2))
-    yi_2to1 <- suppressMessages(predictfunc(mod, xi_2to1))
+    # yi_1to2 <- suppressMessages(predictfunc(mod, xi_1to2))
+    # yi_2to1 <- suppressMessages(predictfunc(mod, xi_2to1))
+    yi_1to2 <- predictfuncwithmod(xi_1to2)
+    yi_2to1 <- predictfuncwithmod(xi_2to1)
     df <- rbind(df, data.frame(i=i, name=colnames(x1)[i],
                                y1to2 = yi_1to2,
                                y2to1 = yi_2to1
